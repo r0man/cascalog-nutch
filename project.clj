48ab28b4892b71d129f1eaa58682383329b6a84c
Roman <roman@burningswell.com>
2012-03-05 22:37:00 +0100

    Initial import.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject cascading-nutch "0.0.1-SNAPSHOT"
+  :description "Cascading Scheme for Nutch sequence files."
+  :dependencies [[cascading/cascading-core "1.2.4"
+                  :exclusions [org.codehaus.janino/janino
+                               thirdparty/jgrapht-jdk1.6
+                               riffle/riffle]]
+                 [org.apache.nutch/nutch "1.4"]])

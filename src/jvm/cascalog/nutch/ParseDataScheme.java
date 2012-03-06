db47b63ff7c7781f3cfb49e06e1dfe847ba7632f
Roman <roman@burningswell.com>
2012-03-06 22:07:09 +0100

    Added nutch taps.

--- src/jvm/cascalog/nutch/ParseDataScheme.java
+++ src/jvm/cascalog/nutch/ParseDataScheme.java
@@ -1,34 +1,13 @@
 package cascalog.nutch;
 
-import java.io.IOException;
-
-import org.apache.hadoop.mapred.OutputCollector;
-
-import cascading.scheme.SequenceFile;
 import cascading.tuple.Fields;
-import cascading.tuple.Tuple;
-import cascading.tuple.TupleEntry;
 
-public class ParseDataScheme extends SequenceFile {
+public class ParseDataScheme extends NutchScheme {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public ParseDataScheme() {
-		super(new Fields("url", "parse-data"));
-	}
-
-	@SuppressWarnings("rawtypes")
-	@Override
-	public void sink(TupleEntry tupleEntry, OutputCollector outputCollector) throws IOException {
-		throw new UnsupportedOperationException("Sink not yet imlemented.");
-	}
-	
-	@Override
-	public Tuple source(Object key, Object value) {
-		Tuple tuple = new Tuple();
-		tuple.add(key);
-		tuple.add(value);
-		return tuple; 		
-	}
+    public ParseDataScheme() {
+        super(new Fields("url", "parse-data"));
+    }
 
 }
--- src/jvm/cascalog/nutch/ParseTextScheme.java
+++ src/jvm/cascalog/nutch/ParseTextScheme.java
@@ -1,34 +1,13 @@
 package cascalog.nutch;
 
-import java.io.IOException;
-
-import org.apache.hadoop.mapred.OutputCollector;
-
-import cascading.scheme.SequenceFile;
 import cascading.tuple.Fields;
-import cascading.tuple.Tuple;
-import cascading.tuple.TupleEntry;
 
-public class ParseTextScheme extends SequenceFile {
+public class ParseTextScheme extends NutchScheme {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public ParseTextScheme() {
-		super(new Fields("url", "parse-text"));
-	}
-
-	@SuppressWarnings("rawtypes")
-	@Override
-	public void sink(TupleEntry tupleEntry, OutputCollector outputCollector) throws IOException {
-		throw new UnsupportedOperationException("Sink not yet imlemented.");
-	}
-	
-	@Override
-	public Tuple source(Object key, Object value) {
-		Tuple tuple = new Tuple();
-		tuple.add(key);
-		tuple.add(value);
-		return tuple; 		
-	}
+    public ParseTextScheme() {
+        super(new Fields("url", "parse-text"));
+    }
 
 }

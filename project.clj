752afb0cfad34dc7f31a0ecdddc3630a03634e2c
Roman Scherer <roman.scherer@nugg.ad>
2012-03-29 11:37:48 +0200

    Use Leiningen's javac task.

--- project.clj
+++ project.clj
@@ -4,8 +4,5 @@
                  [org.apache.nutch/nutch "1.4"]
                  [org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]]
-  :plugins [[lein-javac "1.3.0"]]
-  :hooks [leiningen.hooks.javac]
   :source-path "src/clj"
-  :javac-source-path [["src/jvm"]]
-  :javac-options {:debug "true"})
+  :java-source-path "src/jvm")

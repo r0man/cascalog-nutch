aaf41126792560027823dbac172bf7ba90368c0b
Roman Scherer <roman.scherer@nugg.ad>
2012-03-29 11:39:17 +0200

    Add parse text as string, instead of text.

--- src/jvm/cascalog/nutch/ParseTextScheme.java
+++ src/jvm/cascalog/nutch/ParseTextScheme.java
@@ -1,6 +1,7 @@
 package cascalog.nutch;
 
 import cascading.tuple.Fields;
+import cascading.tuple.Tuple;
 
 public class ParseTextScheme extends NutchScheme {
 
@@ -8,6 +9,14 @@ public class ParseTextScheme extends NutchScheme {
 
     public ParseTextScheme() {
         super(new Fields("url", "parse-text"));
+    }
+
+    @Override
+    public Tuple source(Object key, Object value) {
+        Tuple tuple = new Tuple();
+        tuple.add(key.toString());
+        tuple.add(value.toString());
+        return tuple;
     }
 
 }

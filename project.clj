dbd74ef1fa9348d87b24a25a4c03be530969e7f1
Roman Scherer <roman.scherer@nugg.ad>
2012-04-17 10:25:21 +0200

    Switched to Clojure 1.4.0.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Cascading Schemes and Cascalog helpers for Nutch sequence files."
   :dependencies [[cascalog "1.8.6"]
                  [org.apache.nutch/nutch "1.4"]
-                 [org.clojure/clojure "1.3.0"]]
+                 [org.clojure/clojure "1.4.0"]]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]]
   :source-path "src/clj"
   :java-source-path "src/jvm"

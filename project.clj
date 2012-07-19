20f5d0dae8e25c3ee7b215cb9642de24c991c6d1
Roman Scherer <roman.scherer@nugg.ad>
2012-07-19 11:46:37 +0200

    Switched to cascalog-more-taps 0.3.0-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject cascalog-nutch "0.0.3-SNAPSHOT"
   :description "Cascading Schemes and Cascalog helpers for Nutch sequence files."
   :dependencies [[cascalog "1.10.0"]
-                 [cascalog-more-taps "0.2.1"]
+                 [cascalog-more-taps "0.3.0-SNAPSHOT"]
                  [org.apache.nutch/nutch "1.5"]
                  [org.clojure/clojure "1.4.0"]]
   :profiles {:dev {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-cdh3u3"]]}}
